de7974c993ebf5443f8210c886f45cff3d92426c
galangprabowo681-arch <galangprabowo681@gmail.com>
2026-01-21 22:17:56 +0700

    Add files via upload

---
+++ inventory-app/migrations/xxxx-create-users.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      role: {
+        type: Sequelize.STRING,
+        allowNull: false
+      }
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('users');
+  }
+};
